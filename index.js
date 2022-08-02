eba81c1122b456afa2b9a31b2d771d42eab0953b
RHShoumik <rhshoumik12@gmail.com>
2022-08-03 00:00:48 +0600

    )2

--- index.js
+++ index.js
@@ -218,14 +218,14 @@
 // }
 
 
-showStars(10);
+// showStars(10);
 
-function showStars(rows){
-    for(let i= 1; i<= rows; i++){
-       let pattern = '';
-       for(let j=0; j< i ; j++){
-           pattern += '*';
-       } 
-       console.log(pattern);
-    }
-}
+// function showStars(rows){
+//     for(let i= 1; i<= rows; i++){
+//        let pattern = '';
+//        for(let j=0; j< i ; j++){
+//            pattern += '*';
+//        } 
+//        console.log(pattern);
+//     }
+// }
